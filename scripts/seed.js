8bed4e9cc45577989746bdf2d79de280f6bf227a
Stuart <stuart.haas2@gmail.com>
2021-08-20 11:25:25 -0400

    Improve seed script

--- scripts/seed.js
+++ scripts/seed.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const chalk = require('chalk');
 const { prompt, Select, NumberPrompt } = require('enquirer');
 const Product = require('../src/models/product.model');
+const Category = require('../src/models/category.model');
 const faker = require('faker');
 
 async function setup() {
@@ -22,10 +23,10 @@ async function setup() {
     const collectionPrompt = new Select({
       name: 'collection',
       message: 'Choose a collection',
-      choices: ['Products', 'Categories', 'Users'],
+      choices: ['Products', 'Categories'],
     });
 
-    await collectionPrompt.run();
+    const collectionChoice = await collectionPrompt.run();
 
     const numberPrompt = new NumberPrompt({
       name: 'number',
@@ -34,15 +35,35 @@ async function setup() {
 
     const numberChoice = await numberPrompt.run();
 
-    let documents = [];
-    for (let i = 0; i < numberChoice; i += 1) {
-      const product = getProduct();
-      documents.push(product);
+    if(collectionChoice === 'Products') {
+      let documents = [];
+      for (let i = 0; i < numberChoice; i += 1) {
+        const product = await getProduct();
+        documents.push(product);
+      }
+
+      try {
+        const products = await Product.insertMany(documents);
+        console.log(`${products.length} products inserted`);
+      } catch(e) {
+        console.log(e);
+      }
     }
 
-    const products = await Product.insertMany(documents);
+    if(collectionChoice === 'Categories') {
+      let documents = [];
+      for (let i = 0; i < numberChoice; i += 1) {
+        const category = await getCategory();
+        documents.push(category);
+      }
 
-    console.log(products);
+      try {
+        const categories = await Category.insertMany(documents);
+        console.log(`${categories.length} categories inserted`);
+      } catch(e) {
+        console.log(e);
+      }
+    }
 
     process.kill(process.pid, 'SIGTERM');
   } catch (error) {
@@ -51,7 +72,9 @@ async function setup() {
   }
 }
 
-function getProduct() {
+async function getProduct() {
+  const categories = await Category.find();
+  const category = categories[Math.floor(Math.random() * categories.length)];
   const name = faker.commerce.productName();
   const sku = faker.random.uuid();
   const description = faker.commerce.productDescription();
@@ -60,7 +83,16 @@ function getProduct() {
   const image = `https://placeimg.com/640/480/tech?random=${Math.round(
     Math.random() * 1000
   )}`;
-  return { name, sku, description, price, inventory, image };
+  return { name, sku, description, price, inventory, image, category: category.id };
+}
+
+async function getCategory() {
+  const name = faker.commerce.productName();
+  const image = `https://placeimg.com/640/480/tech?random=${Math.round(
+    Math.random() * 1000
+  )}`;
+  const status = 'active';
+  return { name, image, status };
 }
 
 setup();
